22e4c10fdb926a7d74da3d6223543412080a0759
LiamPCostello <lcostello05@gmail.com>
2023-10-11 20:30:01 +0100

    commeneted out code

--- rightTriangle.java
+++ rightTriangle.java
@@ -1,5 +1,5 @@
 public class rightTriangle {
     public static void main(String[] args) {
-        System.out.println("Enter a number: ");
+       //System.out.println("Enter a number: ");
     }
 }
